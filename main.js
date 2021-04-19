514b2bdef01544f576a04f851a0182ccc4377372
Juan Vitale <juanpablovitale95@gmail.com>
2021-04-19 18:19:48 -0300

    objetos

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 
 
-const nombres = ["God of War", "GTA V", "Call of Duty; Modern Warafare"];
+let nombres = ["God of War", "GTA V", "Call of Duty: Modern Warafare"];
 let precios = [2000, 1600, 4500];
 let imagenes = ['https://images-na.ssl-images-amazon.com/images/I/813xlI-NGpL._SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/916T5H6sCtL._SL1500_.jpg', 'https://images-na.ssl-images-amazon.com/images/I/81n2llGXyiL._SL1500_.jpg'];
 let cardsHome = ``;
 let totalCarrito = 0;
+
+//Objetos
+class producto{
+    constructor(nombre, precio, imagen){
+        this.nombre = nombre;
+        this.precio = precio; 
+        this.imagen = imagen; 
+    }
+}
+
+const productoUno = new producto(`God of War`, 2000, `https://images-na.ssl-images-amazon.com/images/I/813xlI-NGpL._SL1500_.jpg`);
+const productoDos = new producto(`GTA V`, 1600, `https://images-na.ssl-images-amazon.com/images/I/916T5H6sCtL._SL1500_.jpg` );
+const productoTres = new producto(`Call of Duty: Modern Warfare`, 4500, 'https://images-na.ssl-images-amazon.com/images/I/81n2llGXyiL._SL1500_.jpg');
+
+const productoImagen = [productoUno.imagen, productoDos.imagen, productoTres.imagen];
+const productoNombre = [productoUno.nombre, productoDos.nombre, productoTres.nombre];
+const productoPrecio = [productoUno.precio, productoDos.precio, productoTres.precio];
 //Productos Home
 
 for (let i = 0; i < nombres.length; i++){
     cardsHome+= `<div class="col-lg-4 col-md-6 mb-4">
                     <div class="card h-100">
-                        <a href="#"><img class="card-img-top" src=${imagenes[i]} alt=""></a>
+                        <a href="#"><img class="card-img-top" src=${productoImagen[i]} alt=""></a>
                         <div class="card-body">
                             <h4>
-                                ${nombres[i]}
+                                ${productoNombre[i]}
                             </h4>
-                            <h5>$ ${precios[i]}</h5>
+                            <h5>$ ${productoPrecio[i]}</h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                            <button class="btn-primary" onclick= agregarAlCarrito(${precios[i]})>Agregar al Carrito</button>
-                            <button class="btn-primary mt-1" onclick = calcularCuotas(${precios[i]})>Calcular cuotas</button>
+                            <button class="btn-primary" onclick= agregarAlCarrito(${productoPrecio[i]})>Agregar al Carrito</button>
+                            <button class="btn-primary mt-1" onclick = calcularCuotas(${productoPrecio[i]})>Calcular cuotas</button>
                         </div>
                         <div class="card-footer">
                             <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
